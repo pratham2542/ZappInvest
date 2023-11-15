1ca684e23d2ded3193ab72e24281a4ac3cb2e862
Pratham <pratham@pop-os.localdomain>
2023-11-15 15:22:29 +0530

    updated_Screen_2

--- Screens/MainScreen/prathamscreens/screen2.js
+++ Screens/MainScreen/prathamscreens/screen2.js
@@ -13,6 +13,7 @@ import React, { useState, useEffect } from "react";
 import colors from "../../../config/colors";
 import AppButton from "../../../components/utils/AppButton";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { LinearGradient } from "expo-linear-gradient";
 
 const HomeScreen = ({ navigation }) => {
   const dimensions = Dimensions.get("window");
@@ -70,15 +71,15 @@ const HomeScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("startupForm")}
             />
           </View>
-          <View style={{ marginBottom: 5 ,width: "40%",borderRadius:10}}>
+          <View style={{ marginBottom: 5, width: "40%", borderRadius: 10 }}>
             <TouchableOpacity
               style={{
                 borderRadius: 10,
                 justifyContent: "center",
                 alignItems: "center",
                 padding: 15,
-                
-                backgroundColor:"#f1f1f1"
+
+                backgroundColor: "#f1f1f1",
               }}
               onPress={() => navigation.navigate("startupForm")}
             >
@@ -88,7 +89,7 @@ const HomeScreen = ({ navigation }) => {
                   fontSize: 12,
                   textTransform: "uppercase",
                   fontWeight: "bold",
-                  color:" rgb(119, 119, 119)"
+                  color: " rgb(119, 119, 119)",
                 }}
               >
                 Login
@@ -107,53 +108,78 @@ const HomeScreen = ({ navigation }) => {
 
       {/* section 2 */}
 
-      <View style={{ marginHorizontal: 20 ,marginTop:15}}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>
-            Why Join Us ?
-        </Text>
+      <View style={{ marginHorizontal: 20, marginTop: 15 }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>Why Join Us ?</Text>
         <Text style={{ marginTop: 10, fontSize: 15 }}>
-            Zapp provide you a way of raising capital from your customers and make them part of your startup.
+          Zapp provide you a way of raising capital from your customers and make
+          them part of your startup.
         </Text>
       </View>
 
-      <View style={{paddingHorizontal:20,marginBottom:30}}>
-        <View style={{backgroundColor:"rgba(64, 164, 255, 0.1)",borderRadius:10,marginTop:20,padding:20}}>
-            <View>
-                <Text style={{fontSize:18,fontWeight:"bold"}}>
-                    Raise capital with ease
-                </Text>
-            </View>
-            <View>
-                <Text>
-                    Raise capital for your startup with ease and grow your startup with right mentor based on your sector and current requirement and wider audience.
-                </Text>
-            </View>
+      <View style={{ paddingHorizontal: 20, marginBottom: 30 }}>
+        <View
+          style={{
+            backgroundColor: "rgba(64, 164, 255, 0.1)",
+            borderRadius: 10,
+            marginTop: 20,
+            padding: 20,
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+              Raise capital with ease
+            </Text>
+          </View>
+          <View>
+            <Text>
+              Raise capital for your startup with ease and grow your startup
+              with right mentor based on your sector and current requirement and
+              wider audience.
+            </Text>
+          </View>
         </View>
-        <View style={{backgroundColor:"rgba(64, 164, 255, 0.1)",borderRadius:10,marginTop:20,padding:20}}>
-            <View>
-                <Text style={{fontSize:18,fontWeight:"bold"}}>
-                    Raise capital with ease
-                </Text>
-            </View>
-            <View>
-                <Text>
-                    Raise capital for your startup with ease and grow your startup with right mentor based on your sector and current requirement and wider audience.
-                </Text>
-            </View>
+        <View
+          style={{
+            backgroundColor: "rgba(64, 164, 255, 0.1)",
+            borderRadius: 10,
+            marginTop: 20,
+            padding: 20,
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+              Raise capital with ease
+            </Text>
+          </View>
+          <View>
+            <Text>
+              Raise capital for your startup with ease and grow your startup
+              with right mentor based on your sector and current requirement and
+              wider audience.
+            </Text>
+          </View>
         </View>
-        <View style={{backgroundColor:"rgba(64, 164, 255, 0.1)",borderRadius:10,marginTop:20,padding:20}}>
-            <View>
-                <Text style={{fontSize:18,fontWeight:"bold"}}>
-                    Raise capital with ease
-                </Text>
-            </View>
-            <View>
-                <Text>
-                    Raise capital for your startup with ease and grow your startup with right mentor based on your sector and current requirement and wider audience.
-                </Text>
-            </View>
+        <View
+          style={{
+            backgroundColor: "rgba(64, 164, 255, 0.1)",
+            borderRadius: 10,
+            marginTop: 20,
+            padding: 20,
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+              Raise capital with ease
+            </Text>
+          </View>
+          <View>
+            <Text>
+              Raise capital for your startup with ease and grow your startup
+              with right mentor based on your sector and current requirement and
+              wider audience.
+            </Text>
+          </View>
         </View>
-       
       </View>
 
       {/* section 3 */}
@@ -363,7 +389,326 @@ const HomeScreen = ({ navigation }) => {
           )}
         </View>
       </View>
-      <View style={{ backgroundColor: "#eee" }}></View>
+
+      {/* section4 */}
+
+      <View
+        style={
+          {
+            // backgroundColor:"blue",
+            // backgroundImage:
+            //   "linear-gradient(149deg, rgb(3, 12, 34), rgb(4, 57, 131) 37%, rgb(5, 111, 250))",
+            // backgroundImage: "linear-gradient(to bottom, blue, white)",
+          }
+        }
+      >
+        <LinearGradient
+          colors={["rgb(3, 12, 34)", "rgb(4, 57, 131) 37%","rgb(5, 111, 250)"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0.5, y: 0.5 }}
+          style={{ padding: 20 }}
+        >
+          <Text
+            style={{
+              color: "white",
+              marginTop: 15,
+              fontSize: 25,
+              paddingRight: 15,
+              fontWeight: "bold",
+            }}
+          >
+            How to registeer your startups on Zapp ?
+          </Text>
+
+          <Text style={{ color: "white", paddingTop: 10 }}>
+            Raise capital with eas. Take your startup to new height with Zapp Go
+            through the below chart to understanding how it works
+          </Text>
+          <View style={{ marginTop: 25,paddingHorizontal:25 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                  
+                }}
+              >
+                1
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20 ,paddingRight:50}}>
+                Fill the from available on Zapp
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                2
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20,paddingRight:55 }}>
+                 Zapp team will review the form
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                3
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20 ,paddingRight:55}}>
+                Startup will be live on website
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                4
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20,paddingRight:55 }}>
+                Access curated dashboard
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                5
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20 ,paddingRight:55}}>
+                Completestartup Profile
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                6
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20,paddingRight:55 }}>
+                Dashboard Credential send to startup email
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                alignItems: "center",
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                7
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20,paddingRight:55 }}>
+                Enjoy benifits provided by Zapp
+              </Text>
+            </View>
+            <View style={{height:45,alignItems:"center"}}> 
+                
+                <Text style={{height:"100%",width:1,borderColor:"white",borderWidth:1,borderStyle:"dashed"}}></Text>
+                
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                borderColor: "white",
+                borderWidth: 1.5,
+                borderRadius: 20,
+                padding: 25,
+                borderStyle: "dashed",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  textAlign: "center",
+                  fontSize: 22,
+                  borderRadius: 50,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginRight:20
+                }}
+              >
+                8
+              </Text>
+              <View style={{flexDirection:"column"}}>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20,paddingRight:55 ,marginBottom:10}}>
+                Raise capital for your startup with ease. No complex and tiring process.
+              </Text>
+              <Text style={{ fontWeight:"bold",color: "white", fontSize: 20 ,paddingRight:55}}>
+                Raise through different round available on Zapp such as community round, angle round, private round.
+              </Text>
+              </View>
+              
+            </View>
+          </View>
+        </LinearGradient>
+      </View>
     </ScrollView>
   );
 };
